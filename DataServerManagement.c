8e78188a8658bda736682d9e38e6b773bc2db8bd
aryais86 <n.arya.p2211@gmail.com>
2024-08-27 13:29:32 +0700

    Create switchMenu + error checking opsi

--- DataServerManagement.c
+++ DataServerManagement.c
@@ -1,14 +1,40 @@
 #include<stdio.h>
 
+void switchMenu(int opsi){
+	switch(opsi){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+	}	
+}
+
 void menu(){
-	int opsi;
-	printf("Silahkan Pilih Menu...\n");
-	printf("1.Masukkan Data Server\n");
-	printf("2.Masukkan Data Person\n");
-	printf("3.Masukkan Izin Masuk ke Dalam Server\n");
-	printf("4.Report Akses Server\n");
-	printf(">> ");
-	scanf("%d", &opsi);
+	int opsi=0;
+	
+	do{
+		printf("Silahkan Pilih Menu...\n");
+		printf("1.Masukkan Data Server\n");
+		printf("2.Masukkan Data Person\n");
+		printf("3.Masukkan Izin Masuk ke Dalam Server\n");
+		printf("4.Report Akses Server\n");
+		printf("Exit\n");
+		printf(">> ");
+		scanf("%d", &opsi);
+		
+		if(opsi<1 ||opsi>5){
+			printf("\nPilihan tidak Valid!\n");
+			printf("Silahkan Coba Lagi!\n\n");
+		}else{
+			printf("\n\n");
+			switchMenu(opsi);
+		}
+	}while(opsi<1 ||opsi>5);
+	
 }
 
 int main(){
